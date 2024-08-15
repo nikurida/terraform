2429eee56f39625036d563ec170f982f70922777
Ricardo Araújo almeida <ricardo.almeida@multipedidos.com.br>
2024-08-14 22:52:15 -0400

    add lambda url output

--- setOrdersIsPrinted/src/modules/lambdas/main.tf
+++ setOrdersIsPrinted/src/modules/lambdas/main.tf
@@ -41,3 +41,33 @@ resource "aws_lambda_function" "setIsPrintedLambda_eredis" {
     security_group_ids = [var.security_group_id]
   }
 }
+
+resource "aws_lambda_function_url" "setIsPrintedLambda_sqs_url" {
+  function_name      = aws_lambda_function.setIsPrintedLambda_sqs.function_name
+  qualifier          = "setIsPrintedLambda_sqs"
+  authorization_type = "AWS_IAM"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+}
+
+resource "aws_lambda_function_url" "setIsPrintedLambda_eredis_url" {
+  function_name      = aws_lambda_function.setIsPrintedLambda_eredis.function_name
+  qualifier          = "setIsPrintedLambda_eredis"
+  authorization_type = "AWS_IAM"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+}
--- setOrdersIsPrinted/src/modules/lambdas/outputs.tf
+++ setOrdersIsPrinted/src/modules/lambdas/outputs.tf
@@ -5,3 +5,11 @@ output "setIsPrintedLambda_eredis_arn" {
 output "setIsPrintedLambda_sqs" {
   value = aws_lambda_function.setIsPrintedLambda_sqs.arn
 }
+
+output "setIsPrintedLambda_eredis_url" {
+  value = aws_lambda_function_url.setIsPrintedLambda_eredis_url.url
+}
+
+output "setIsPrintedLambda_sqs_url" {
+  value = aws_lambda_function_url.setIsPrintedLambda_sqs_url.url
+}
